5fbd25c5bd78b18e786683c049e7c0ccc56d9265
moneywar <kong.deesamoot@gmail.com>
2025-01-28 16:08:35 +0700

    Make score count from falledBubble

--- GameObject/GameBoard.cs
+++ GameObject/GameBoard.cs
@@ -164,6 +164,7 @@ namespace bubble_puzzle.GameObject
                 case GameState.BubbleFall:
                     falledBubbles = checkFall();
                     matchedBubbles.Clear();
+                    calculateScore();
 
                     foreach (Bubble bubble in falledBubbles)
                     {
@@ -707,6 +708,11 @@ namespace bubble_puzzle.GameObject
                 }
             }
             group.Add(new Tuple<List<Point>, bool>(groupTemp, isConnectTop));
+        }
+
+        public void calculateScore() 
+        {
+            Singleton.Instance.score += falledBubbles.Count * 5;
         }
     }
 }
